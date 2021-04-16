93b23efed37cb5dd502f482988b3016732ddd687
JottaPé <jp02120123@gmail.com>
2021-04-15 22:12:50 -0300

    localhoste
    First host

---
+++ Projects localhost/data.js
@@ -0,0 +1,42 @@
+module.exports = [
+    {
+        id: "4RiC8wnzVag",
+        title: "Administraçãoes do computador no SO windows apenas pelo prompt de comando  | João Pedro",
+        duration: "12:34min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "aqUHnXGbW98",
+        title: "Todos os comandos do CMD - Prompt de comando | PH Tech",
+        duration: "7:57min",
+        price: "Free"
+    },
+    {
+        id: "8tIYhnWJUPM",
+        title: "REPARANDO A IMAGEM DO WINDOWS PELO CMD!(iniciante) | Alexandre ",
+        duration: " 3:10",
+        price: "Free"
+    },
+    {
+        id: "6vFtsXJ1eTI",
+        title: "Como Usar Comandos PING e IPCONFIG do CMD | Emerson R. Almeida",
+        duration: " 7:49min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "Pq3fe2D0fug",
+        title: "15 cmd hacks for Windows 10 you should know | MoonKing Hackers Club ",
+        duration: "2:25min",
+        price: "Free"
+    },
+    {
+        id: "MBBWVgE0ewk",
+        title: "Windows Command Line Tutorial - 1 - Introduction to the Command Prompt | thenewboston",
+        duration: "7:31min",
+        price: "Free",
+        featured: true
+    }    
+
+]
---
+++ Projects localhost/server.js
@@ -0,0 +1,60 @@
+
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+const videos = require("./data");
+
+server.use(express.static('public'));
+
+server.set("view engine", "njk",);
+nunjucks.configure("views", {
+  express: server,
+  autoescape: false,
+  noCache: true,
+});
+
+server.get("/", function(req, res){
+  const about = { 
+    avatar_url:"https://images8.alphacoders.com/545/thumb-1920-545909.jpg",
+    nome: "Ken kaneki",
+    role: "Ghoul",
+    links: [
+      { nome: "Github", url: "https://github.com/Subpvp"},
+      { nome: "Twitter", url: "https://twitter.com/SubPvP"},
+      { nome: "Youtube", url: "https://www.youtube.com/channel/UCK2deOyGLojnZF9fHzngr6g"},
+      { nome: "Linkedin", url: "https://www.linkedin.com/in/joao-pedro-455b79203/"}
+    ]
+  }
+
+  return res.render("about", { about })
+
+});
+
+server.get("/portfolio", function(req, res){
+  return res.render("portfolio", {items: videos});
+
+});
+
+server.get("/video", function(req, res) {
+  const id = req.query.id
+
+  const video = videos.find(function(video){
+    return video.id == id
+
+  })
+
+  if (!video) {
+    return res.render("Video not found!")
+  }
+  
+  return res.render("video", {item:video})
+
+
+})
+
+
+server.listen(5050, function() {
+  console.log("Server is running");
+
+})
